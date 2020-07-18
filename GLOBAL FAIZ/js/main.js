8809d6d87a0a8e8ef102ed1535698015e8ea2857
Mubarak Shuaib <39945260+Mubaola23@users.noreply.github.com>
2020-07-18 15:20:03 +0100

    Add files via upload

---
+++ GLOBAL FAIZ/js/main.js
@@ -0,0 +1,77 @@
+
+//First faq
+function myFunction(){
+		var x = document.getElementById("myDiv");
+		if (x.style.display === "none"){
+			x.style.display="block";
+		} else{
+			x.style.display = "none";
+		}
+	}
+//second faq
+	function myFunction1(){
+		var x = document.getElementById("myDiv-1");
+		if (x.style.display === "none"){
+			x.style.display="block";
+		} else{
+			x.style.display = "none";
+		}
+	}
+//third faq
+	function myFunction2(){
+		var x = document.getElementById("myDiv-2");
+		if (x.style.display === "none"){
+			x.style.display="block";
+		} else{
+			x.style.display = "none";
+		}
+	}
+
+	//fourth faq
+	function myFunction3(){
+		var x = document.getElementById("myDiv-3");
+		if (x.style.display === "none"){
+			x.style.display="block";
+		} else{
+			x.style.display = "none";
+		}
+	}
+//fifth faq
+	function myFunction4(){
+		var x = document.getElementById("myDiv-4");
+		if (x.style.display === "none"){
+			x.style.display="block";
+		} else{
+			x.style.display = "none";
+		}
+	}
+	//sixth faq
+	function myFunction5(){
+		var x = document.getElementById("myDiv-5");
+		if (x.style.display === "none"){
+			x.style.display="block";
+		} else{
+			x.style.display = "none";
+		}
+	}
+	//seventh faq
+	function myFunction6(){
+		var x = document.getElementById("myDiv-6");
+		if (x.style.display === "none"){
+			x.style.display="block";
+		} else{
+			x.style.display = "none";
+		}
+	}
+
+
+
+
+	//function myImg(){
+	//	var y = document.getElementById("image");
+	//	if (y.style.transform === "none"){
+	//		y.style.transform="rotate(90deg)";
+	//	} else{
+	//		y.style.transform = "none";
+	//	}
+	//}
